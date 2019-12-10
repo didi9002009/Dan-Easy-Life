fe5272cac59d887627ae515946d195e15473bc0d
didi9002009 <53850889+didi9002009@users.noreply.github.com>
2019-12-10 18:09:27 +1100

    Update & add images

--- src/index.js
+++ src/index.js
@@ -1,34 +1,36 @@
-jQuery(document).ready(function() {
-  "use strict";
-  $("#slider-carousel").carouFredSel({
-    responsive: true,
-    width: "100%",
-    circular: true,
-    scroll: {
-      items: 1,
-      duration: 500,
-      pauseOnHover: true
-    },
-    auto: true,
-    items: {
-      visible: {
-        min: 1,
-        max: 1
-      },
-      height: "visiable"
-    },
-    pagination: {
-      container: ".sliderpager",
-      pageAnchoBuiler: false
-    }
-  });
-  $(window).scroll(function() {
-    var top = $(window).scrollTop();
-    if (top >= 60) {
-      $("hearder").addClass("secondary");
-    } else if ($("hearder").hasClass("secondary")) {
-      $("hearder").removeClass("secondary");
-    }
-  });
-});
 import "./styles.css";
+window.onload = function() {
+	jQuery(document).ready(function($) {
+		"use strict";
+		$("#slider-carousel").carouFredSel({
+			responsive: true,
+			width: "100%",
+			circular: true,
+			scroll: {
+				items: 1,
+				duration: 500,
+				pauseOnHover: true
+			},
+			auto: true,
+			items: {
+				visible: {
+					min: 1,
+					max: 1
+				},
+				height: "visiable"
+			},
+			pagination: {
+				container: ".sliderpager",
+				pageAnchoBuiler: false
+			}
+		});
+		$(window).scroll(function() {
+			var top = $(window).scrollTop();
+			if (top >= 60) {
+				$("hearder").addClass("secondary");
+			} else if ($("hearder").hasClass("secondary")) {
+				$("hearder").removeClass("secondary");
+			}
+		});
+	});
+};
